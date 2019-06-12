e7d2546ebcef4be47343208a36fe33269d03887b
kaleeswarinataraja <42169965+kaleeswarinataraja@users.noreply.github.com>
2019-06-12 10:53:06 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int l,m,n;
+    scanf("%d %d %d",&a,&b,&c);
+    if(l>m&&l>n)
+    {printf("%d",l);}
+    else if(m>l&&m>n)
+    {printf("%d",m);}
+    else
+    {printf("%d",n);}
+    return 0;
+}
